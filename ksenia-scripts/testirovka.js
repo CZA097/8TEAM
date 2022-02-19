82ec593ec0dfdcf7f293d5080f3f67867d17b1c4
Ksenia <99551590+99ksy@users.noreply.github.com>
2022-02-19 19:25:48 +0300

    Add files via upload

---
+++ ksenia-scripts/testirovka.js
@@ -0,0 +1,12 @@
+HTML CSS JSResult Skip Results Iframe
+EDIT ON
+var colorArray = ["#5A9C6E", "#A8BF5A", "#FAC46E", "#FAD5BB", "#F2FEFF"]; 
+var i = 0; 
+
+function changeColor(){
+    document.body.style.background = colorArray[i]; 
+    i++;
+    if( i > colorArray.length - 1){
+        i = 0;
+    }
+}
